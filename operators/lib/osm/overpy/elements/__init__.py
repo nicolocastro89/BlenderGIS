80deb00a41e67658028fd349a06ab31847d6cd48
nicolo.castro <nicolo.castro@hudl.com>
2024-05-21 10:40:25 +0200

    new elemnts

--- operators/lib/osm/overpy/elements/__init__.py
+++ operators/lib/osm/overpy/elements/__init__.py
@@ -8,6 +8,9 @@ from .element import OSMElement
 from .node import OSMNode
 from .way import OSMWay
 from .railway import OSMRailway
+from .amenity import OSMAmenity
+from .barrier import OSMBarrier
+from .liesure import OSMLeisure
 
 from typing import TypeVar, Literal
 from .. import OSMLibrary
@@ -32,10 +35,11 @@ def ElementFactory(library: OSMLibrary.OSMLibrary, element_definition, data_type
     validation_method_name = f'is_valid_{data_type}'
     creation_method_name = f'load_from_{data_type}'
     valid_types:list[T] = []
-    for possibility in all_subclasses(OSMElement):
+    for possibility in (sc for sc in all_subclasses(OSMElement) if sc.detail_level>=0):
         is_valid = getattr(possibility, validation_method_name)
         if is_valid(element_definition):
             valid_types.append(possibility)
-    valid_types.sort(key=lambda x: x.detail_level, reverse=True)
+    
     if len(valid_types)>0:
+        valid_types.sort(key=lambda x: x.detail_level, reverse=True)
         return getattr(valid_types[0], creation_method_name)(library, element_definition)
